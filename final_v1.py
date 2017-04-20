7ec91b6a3e5e32b618c134ac8a02d6e521ee2b2e
VeniXuan <731851410@qq.com>
2017-04-20 16:46:52 -0400

    model_version1
    The first edition of model to predict delay minutes.

---
+++ final_v1.py
@@ -0,0 +1,30 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+
+
+# load pima indians dataset
+dataset = numpy.loadtxt("Boston.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:5]
+Y = dataset[:,5]
+
+
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='linear'))
+
+# Compile model
+model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['accuracy'],verbose=1)
+
+# Fit the model
+model.fit(X, Y, epochs=300, batch_size=200)
+
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
